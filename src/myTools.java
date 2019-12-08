00ae86f3b028421b10e5474f6da04cf513e79ecb
meiya_baojw <bjw9808@gmail.com>
2019-12-08 15:04:46 +0800

    添加了部分异常处理代码

--- src/myTools.java
+++ src/myTools.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.net.URLDecoder;
@@ -14,11 +15,20 @@ public class myTools extends JFrame {
     private JButton jb_1, jb_2, jb_base_64_en, jb_base_64_de, jb_MD5, jb_url_encode, jb_url_decode;
     private JPanel jpanel;
     private JTextField jtext_1, jtext_2;
+    private JLabel jlabel_input, jlabel_output;
 
     public myTools() {
+        Font myFont=new Font("微软雅黑",Font.BOLD,14);
+        //JPanel init
         jpanel = new JPanel();
-        jtext_1 = new JTextField("输入", 80);
-        jtext_2 = new JTextField("输出", 80);
+        jpanel.setLayout(new FlowLayout(FlowLayout.LEADING, 20, 5));
+        add(jpanel);
+
+        jlabel_input = new JLabel("输入:");
+        jlabel_output = new JLabel("输出:");
+
+        jtext_1 = new JTextField("", 80);
+        jtext_2 = new JTextField("", 80);
 
         jb_1 = new JButton("时间戳(ms)");
         jb_2 = new JButton("时间戳(s)");
@@ -42,7 +52,23 @@ public class myTools extends JFrame {
 
         jb_MD5.addActionListener(new actionMD5encode());
 
+        //统一设置字体
+        jlabel_output.setFont(myFont);
+        jlabel_input.setFont(myFont);
+        jtext_1.setFont(myFont);
+        jtext_2.setFont(myFont);
+        jb_1.setFont(myFont);
+        jb_2.setFont(myFont);
+        jb_url_encode.setFont(myFont);
+        jb_url_decode.setFont(myFont);
+        jb_base_64_de.setFont(myFont);
+        jb_base_64_en.setFont(myFont);
+        jb_MD5.setFont(myFont);
+
+        jpanel.add(jlabel_input);
         jpanel.add(jtext_1);
+
+        jpanel.add(jlabel_output);
         jpanel.add(jtext_2);
 
         jpanel.add(jb_1);
@@ -56,9 +82,9 @@ public class myTools extends JFrame {
         jpanel.add(jb_url_encode);
         jpanel.add(jb_url_decode);
 
-        add(jpanel);
-        setTitle("研发小工具imBobby");
-        setSize(1000, 500);
+
+        setTitle("小工具合集_by_imBobby");
+        setSize(950, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -67,37 +93,57 @@ public class myTools extends JFrame {
     class actionTimeStampMs implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String inputTimeStamp = jtext_1.getText();
-            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String sd = sdf.format(new Date(Long.parseLong(String.valueOf(inputTimeStamp))));
-            jtext_2.setText(sd);
+            if (jtext_1.getText().equals("")) {
+                JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
+            }
+            else {
+                String inputTimeStamp = jtext_1.getText();
+                SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String sd = sdf.format(new Date(Long.parseLong(String.valueOf(inputTimeStamp))));
+                jtext_2.setText(sd);
+            }
         }
     }
 
     class actionTimeStampS implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            Long inputTimeStamp = Long.valueOf(jtext_1.getText());
-            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String sd = sdf.format(new Date(Long.parseLong(String.valueOf(inputTimeStamp * 1000))));
-            jtext_2.setText(sd);
+            if (jtext_1.getText().equals("")) {
+                JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
+            }
+            else {
+                long inputTimeStamp = Long.parseLong(jtext_1.getText());
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String sd = sdf.format(new Date(Long.parseLong(String.valueOf(inputTimeStamp * 1000))));
+                jtext_2.setText(sd);
+            }
         }
     }
 
     class actionBase64Encode implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            byte[] inputString = jtext_1.getText().getBytes();
-            String s = Base64.getEncoder().encodeToString(inputString);
-            jtext_2.setText(s);
+            if (jtext_1.getText().equals("")) {
+                JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
+            }
+            else {
+                byte[] inputString = jtext_1.getText().getBytes();
+                String s = Base64.getEncoder().encodeToString(inputString);
+                jtext_2.setText(s);
+            }
         }
     }
 
     class actionBase64Decode implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            byte[] base64decodedBytes = Base64.getDecoder().decode(jtext_2.getText());
-            jtext_1.setText(new String(base64decodedBytes));
+            if (jtext_1.getText().equals("")) {
+                JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
+            }
+            else {
+                byte[] base64decodedBytes = Base64.getDecoder().decode(jtext_1.getText());
+                jtext_2.setText(new String(base64decodedBytes));
+            }
         }
     }
 
@@ -124,31 +170,41 @@ public class myTools extends JFrame {
                 jtext_2.setText(buf.toString());
             }
             catch (Exception exception) {
-                System.out.println(exception);
+                JOptionPane.showMessageDialog(jpanel,"异常信息" + exception,"MD5异常",2);
             }
         }
     }
 
     class actionURLEncode implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            String url = jtext_1.getText();
-            try {
-                url = URLEncoder.encode(url, "utf-8");
-            } catch (UnsupportedEncodingException ex) {
-                ex.printStackTrace();
+            if (jtext_1.getText().equals("")) {
+                JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
             }
-            jtext_2.setText(url);
+            else {
+                String url = jtext_1.getText();
+                try {
+                    url = URLEncoder.encode(url, "utf-8");
+                } catch (UnsupportedEncodingException ex) {
+                    ex.printStackTrace();
+                }
+                jtext_2.setText(url);
+            }
         }
     }
 
     class actionURLDecode implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String url_encode = jtext_1.getText();
-            try {
-                jtext_2.setText(URLDecoder.decode(url_encode, "UTF-8"));
-            } catch (UnsupportedEncodingException ex) {
-                ex.printStackTrace();
+            if (jtext_1.getText().equals("")) {
+                JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
+            }
+            else {
+                String url_encode = jtext_1.getText();
+                try {
+                    jtext_2.setText(URLDecoder.decode(url_encode, "UTF-8"));
+                } catch (UnsupportedEncodingException ex) {
+                    ex.printStackTrace();
+                }
             }
         }
     }
