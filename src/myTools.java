dc227477dc5b4377797009d3d1f2dbfb4c642fbb
imBobby <bjw9808@gmail.com>
2019-12-08 22:28:02 +0800

    更改变量名法

--- src/myTools.java
+++ src/myTools.java
@@ -12,10 +12,10 @@ import java.text.SimpleDateFormat;
 import java.util.Base64;
 
 public class myTools extends JFrame {
-    private JButton jb_1, jb_2, jb_base_64_en, jb_base_64_de, jb_MD5, jb_url_encode, jb_url_decode;
+    private JButton jButton1, jButton2, jButtonBase64En, jButtonBase64De, jButtonMD5, jButtonUrlEn, jButtonUrlDe;
     private JPanel jpanel;
-    private JTextField jtext_1, jtext_2;
-    private JLabel jlabel_input, jlabel_output;
+    private JTextField jText1, jText2;
+    private JLabel jLabelIn, jLabelOut;
 
     public myTools() {
         Font myFont=new Font("微软雅黑",Font.BOLD,14);
@@ -24,63 +24,63 @@ public class myTools extends JFrame {
         jpanel.setLayout(new FlowLayout(FlowLayout.LEADING, 20, 5));
         add(jpanel);
 
-        jlabel_input = new JLabel("输入:");
-        jlabel_output = new JLabel("输出:");
+        jLabelIn = new JLabel("输入:");
+        jLabelOut = new JLabel("输出:");
 
-        jtext_1 = new JTextField("", 80);
-        jtext_2 = new JTextField("", 80);
+        jText1 = new JTextField("", 80);
+        jText2 = new JTextField("", 80);
 
-        jb_1 = new JButton("时间戳(ms)");
-        jb_2 = new JButton("时间戳(s)");
+        jButton1 = new JButton("时间戳(ms)");
+        jButton2 = new JButton("时间戳(s)");
 
-        jb_base_64_en = new JButton("base64加密");
-        jb_base_64_de = new JButton("base64解密");
+        jButtonBase64En = new JButton("base64加密");
+        jButtonBase64De = new JButton("base64解密");
 
-        jb_MD5 = new JButton("MD5");
+        jButtonMD5 = new JButton("MD5");
 
-        jb_url_encode = new JButton("url编码");
-        jb_url_decode = new JButton("url解码");
+        jButtonUrlEn = new JButton("url编码");
+        jButtonUrlDe = new JButton("url解码");
 
-        jb_1.addActionListener(new actionTimeStampMs());
-        jb_2.addActionListener(new actionTimeStampS());
+        jButton1.addActionListener(new actionTimeStampMs());
+        jButton2.addActionListener(new actionTimeStampS());
 
-        jb_base_64_en.addActionListener(new actionBase64Encode());
-        jb_base_64_de.addActionListener(new actionBase64Decode());
+        jButtonBase64En.addActionListener(new actionBase64Encode());
+        jButtonBase64De.addActionListener(new actionBase64Decode());
 
-        jb_url_encode.addActionListener(new actionURLEncode());
-        jb_url_decode.addActionListener(new actionURLDecode());
+        jButtonUrlEn.addActionListener(new actionURLEncode());
+        jButtonUrlDe.addActionListener(new actionURLDecode());
 
-        jb_MD5.addActionListener(new actionMD5encode());
+        jButtonMD5.addActionListener(new actionMD5encode());
 
         //统一设置字体
-        jlabel_output.setFont(myFont);
-        jlabel_input.setFont(myFont);
-        jtext_1.setFont(myFont);
-        jtext_2.setFont(myFont);
-        jb_1.setFont(myFont);
-        jb_2.setFont(myFont);
-        jb_url_encode.setFont(myFont);
-        jb_url_decode.setFont(myFont);
-        jb_base_64_de.setFont(myFont);
-        jb_base_64_en.setFont(myFont);
-        jb_MD5.setFont(myFont);
+        jLabelOut.setFont(myFont);
+        jLabelIn.setFont(myFont);
+        jText1.setFont(myFont);
+        jText2.setFont(myFont);
+        jButton1.setFont(myFont);
+        jButton2.setFont(myFont);
+        jButtonUrlEn.setFont(myFont);
+        jButtonUrlDe.setFont(myFont);
+        jButtonBase64De.setFont(myFont);
+        jButtonBase64En.setFont(myFont);
+        jButtonMD5.setFont(myFont);
 
-        jpanel.add(jlabel_input);
-        jpanel.add(jtext_1);
+        jpanel.add(jLabelIn);
+        jpanel.add(jText1);
 
-        jpanel.add(jlabel_output);
-        jpanel.add(jtext_2);
+        jpanel.add(jLabelOut);
+        jpanel.add(jText2);
 
-        jpanel.add(jb_1);
-        jpanel.add(jb_2);
+        jpanel.add(jButton1);
+        jpanel.add(jButton2);
 
-        jpanel.add(jb_base_64_en);
-        jpanel.add(jb_base_64_de);
+        jpanel.add(jButtonBase64En);
+        jpanel.add(jButtonBase64De);
 
-        jpanel.add(jb_MD5);
+        jpanel.add(jButtonMD5);
 
-        jpanel.add(jb_url_encode);
-        jpanel.add(jb_url_decode);
+        jpanel.add(jButtonUrlEn);
+        jpanel.add(jButtonUrlDe);
 
 
         setTitle("小工具合集_by_imBobby");
@@ -93,19 +93,19 @@ public class myTools extends JFrame {
     class actionTimeStampMs implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (jtext_1.getText().equals("")) {
+            if (jText1.getText().equals("")) {
                 JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
             }
             else {
-                String inputTimeStamp = jtext_1.getText();
+                String inputTimeStamp = jText1.getText();
                 SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 try {
                     String sd = sdf.format(new Date(Long.parseLong(String.valueOf(inputTimeStamp))));
-                    jtext_2.setText(sd);
+                    jText2.setText(sd);
                 }
                 catch (NumberFormatException error) {
                     JOptionPane.showMessageDialog(jpanel,"输入时间戳错误，请检查","警告",2);
-                    jtext_1.setText("");
+                    jText1.setText("");
                 }
             }
         }
@@ -114,19 +114,19 @@ public class myTools extends JFrame {
     class actionTimeStampS implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (jtext_1.getText().equals("")) {
+            if (jText1.getText().equals("")) {
                 JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
             }
             else {
                 try {
-                    long inputTimeStamp = Long.parseLong(jtext_1.getText());
+                    long inputTimeStamp = Long.parseLong(jText1.getText());
                     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                     String sd = sdf.format(new Date(Long.parseLong(String.valueOf(inputTimeStamp * 1000))));
-                    jtext_2.setText(sd);
+                    jText2.setText(sd);
                 }
                 catch (Exception error) {
                     JOptionPane.showMessageDialog(jpanel,"输入时间戳错误，请检查","警告",2);
-                    jtext_1.setText("");
+                    jText1.setText("");
                 }
             }
         }
@@ -135,13 +135,13 @@ public class myTools extends JFrame {
     class actionBase64Encode implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (jtext_1.getText().equals("")) {
+            if (jText1.getText().equals("")) {
                 JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
             }
             else {
-                byte[] inputString = jtext_1.getText().getBytes();
+                byte[] inputString = jText1.getText().getBytes();
                 String s = Base64.getEncoder().encodeToString(inputString);
-                jtext_2.setText(s);
+                jText2.setText(s);
             }
         }
     }
@@ -149,12 +149,12 @@ public class myTools extends JFrame {
     class actionBase64Decode implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (jtext_1.getText().equals("")) {
+            if (jText1.getText().equals("")) {
                 JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
             }
             else {
-                byte[] base64decodedBytes = Base64.getDecoder().decode(jtext_1.getText());
-                jtext_2.setText(new String(base64decodedBytes));
+                byte[] base64decodedBytes = Base64.getDecoder().decode(jText1.getText());
+                jText2.setText(new String(base64decodedBytes));
             }
         }
     }
@@ -162,7 +162,7 @@ public class myTools extends JFrame {
     class actionMD5encode implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String encodeText = jtext_1.getText();
+            String encodeText = jText1.getText();
             try{
                 MessageDigest md = MessageDigest.getInstance("MD5");
                 md.update(encodeText.getBytes());
@@ -179,7 +179,7 @@ public class myTools extends JFrame {
                     }
                     buf.append(Integer.toHexString(i));
                 }
-                jtext_2.setText(buf.toString());
+                jText2.setText(buf.toString());
             }
             catch (Exception exception) {
                 JOptionPane.showMessageDialog(jpanel,"异常信息" + exception,"MD5异常",2);
@@ -189,17 +189,17 @@ public class myTools extends JFrame {
 
     class actionURLEncode implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            if (jtext_1.getText().equals("")) {
+            if (jText1.getText().equals("")) {
                 JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
             }
             else {
-                String url = jtext_1.getText();
+                String url = jText1.getText();
                 try {
                     url = URLEncoder.encode(url, "utf-8");
                 } catch (UnsupportedEncodingException ex) {
                     ex.printStackTrace();
                 }
-                jtext_2.setText(url);
+                jText2.setText(url);
             }
         }
     }
@@ -207,13 +207,13 @@ public class myTools extends JFrame {
     class actionURLDecode implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            if (jtext_1.getText().equals("")) {
+            if (jText1.getText().equals("")) {
                 JOptionPane.showMessageDialog(jpanel,"输入禁止为空","警告",2);
             }
             else {
-                String url_encode = jtext_1.getText();
+                String url_encode = jText1.getText();
                 try {
-                    jtext_2.setText(URLDecoder.decode(url_encode, "UTF-8"));
+                    jText2.setText(URLDecoder.decode(url_encode, "UTF-8"));
                 } catch (UnsupportedEncodingException ex) {
                     ex.printStackTrace();
                 }
